c34771219c72690cfa579ac3ff74638666288999
vzl-rbi <screwup39@gmail.com>
2025-10-27 17:24:55 +0545

    Searching product made

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -16,14 +16,21 @@ export const getGenres = async (req, res) => {
 export const getProducts = async (req, res) => {
   try {
     const db = await getDBConnection();
-    const { genre } = req.query;
 
     let query = "SELECT * FROM products";
-    const params = [];
+    let params = [];
+
+    const { genre, search } = req.query;
+
     if (genre) {
-      query += " WHERE genre=?";
+      query += " WHERE genre = ?";
       params.push(genre);
+    } else if (search) {
+      query += " WHERE title LIKE ? OR artist LIKE ? OR genre LIKE ?";
+      const searchPattern = `%${search}%`;
+      params.push(searchPattern, searchPattern, searchPattern);
     }
+
     const products = await db.all(query, params);
 
     res.json(products);
