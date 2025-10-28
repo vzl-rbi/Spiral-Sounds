0ff87aaa7edb68a31e383b3e7c5ac96fecce2ae8
vzl-rbi <screwup39@gmail.com>
2025-10-28 16:47:35 +0545

    Server.js Modified : minor code modifictaion

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const app = express();
 const PORT = 8000;
 app.use(express.static("public"));
 app.use("/api/products", productRouter);
-app.get("/", (req, res) => {
-  res.send(`<!doctype html><html><body> Spiral Sounds </body></html>`);
-});
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+// app.get("/", (req, res) => {
+//   res.send(`<!doctype html><html><body> Spiral Sounds </body></html>`);
+// });
+app
+  .listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  })
+  .on("error", (err) => {
+    console.error("Failed to start server:", err);
+  });
